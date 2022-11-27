fa1f2820b4b529aab5172ca945e15cdd84c1ac08
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2022-11-27 22:14:47 +0100

    Feat: Add properties and constructor for Student class

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -1,7 +1,25 @@
+using HogwartsHouses.Models.Types;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Student
     {
+        public static int idSeed { get; set; }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public HouseType House { get; set; }
+        public PetType Pet { get; set; }
+
+        public Student(string firstName, string lastName, HouseType house, PetType pet)
+        {
+            this.Id = idSeed;
+            idSeed++;
+            FirstName = firstName;
+            LastName = lastName;
+            House = house;
+            Pet = pet;
+        }
     }
 }
