18c07724d32f62efc0334e13a69f9ba8e364960c
HAJNALKA TÓOTH <tothhajnalka17@gmail.com>
2022-11-27 20:08:39 +0100

    Feat: Add working Dependency Injection of RoomService and /Rooms route
    RoomService injection returns all rooms to /rooms route

--- src/Controllers/GreetingController.cs
+++ src/Controllers/GreetingController.cs
@@ -1,29 +1,14 @@
-﻿using HogwartsHouses.DAL;
-using HogwartsHouses.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace HogwartsHouses.Controllers
 {
     [ApiController, Route("/")]
     public class GreetingController : ControllerBase
     {
-        private readonly IRoomService _roomService;
-
-        public GreetingController(IRoomService RoomService)
-        {
-            _roomService = RoomService;
-        }
         [HttpGet("{name?}")]
         public string Greeting(string name = "Witches and Wizards")
         {
             return $"Welcome to Hogwarts, {name}";
-        }
-
-        [HttpGet("rooms")]
-        public List<Room> LocusOmnis()
-        {
-            return _roomService.ListAllRooms();
         }
     }
 }
